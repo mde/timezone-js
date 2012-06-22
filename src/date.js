320dfd65f1fa2737ecf8e8722ff505489b5ec9fb
Ilya Tsuryev <i.tsuryev@gmail.com>
2012-06-23 00:08:29 +0300

    * fix type

--- src/date.js
+++ src/date.js
@@ -57,7 +57,7 @@
     , DAYS = timezoneJS.Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     , MONTHS = timezoneJS.Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     , SHORT_MONTHS = {}
-    , SHORT_DAYS = {},
+    , SHORT_DAYS = {}
     , PARSED_TIME_STRINGS = {};
 
   //`{ "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3, "May": 4, "Jun": 5, "Jul": 6, "Aug": 7, "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11 }`
