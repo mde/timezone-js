371ff710bef13e141481957b517bbca4a5d0cb99
Long Ho <holevietlong@gmail.com>
2013-02-15 11:12:24 -0500

    small tweak

--- src/date.js
+++ src/date.js
@@ -602,7 +602,7 @@
     }
     function getBasicOffset(time) {
       var off = parseTimeString(time)
-        , adj = time.indexOf('-') === 0 ? -1 : 1;
+        , adj = time.charAt(0) === '-' ? -1 : 1;
       off = adj * (((off[1] * 60 + off[2]) * 60 + off[3]) * 1000);
       return off/60/1000;
     }
