7bf0f7797cbe13db2ed24b497e4e3e5def89eccd
Casey Duquette <casey.duquette@hulu.com>
2013-11-15 13:58:39 -0800

    Add tests to verify the format trimming and padding work as expected

--- src/date.js
+++ src/date.js
@@ -122,7 +122,7 @@
     var s = number.toString();
     var s_len = s.length;
     if (s_len > digits) {
-      return s.substr(s_len - digits - 1, s_len);
+      return s.substr(s_len - digits, s_len);
     }
     s = [s];
     while (s_len < digits) {
