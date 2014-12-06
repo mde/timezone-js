b73a871d524226f9af94a7e2fc82147db706db2c
mde <mde@fleegix.org>
2014-12-06 10:23:26 -0800

    Version 0.4.13

--- src/date.js
+++ src/date.js
@@ -55,7 +55,7 @@
     root.timezoneJS = timezoneJS;
   }
 
-  timezoneJS.VERSION = '0.4.12';
+  timezoneJS.VERSION = '0.4.13';
 
   // Grab the ajax library from global context.
   // This can be jQuery, Zepto or fleegix.
